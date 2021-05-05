9a00c85e97e8af9c4f98d8a5f9ab72f5721cb400
Gary Lo <logap@me.com>
2021-05-06 00:58:14 +0800

    add preitter; support delay

--- main.js
+++ main.js
@@ -1,89 +1,81 @@
-const { spawn } = require("child_process");
-const fs = require("fs");
-const { configs } = require(`./${process.argv[2] || "config.json"}`);
+const { spawn } = require('child_process')
+const fs = require('fs')
+const { configs } = require(`./${process.argv[2] || 'config.json'}`)
 
-const startTime = new Date();
-const counter = new Map();
-const processLogMap = new Map();
+const startTime = new Date()
+const counter = new Map()
+const processLogMap = new Map()
 
-let killed = false;
+let killed = false
 
 function startMine(config) {
-  const { id, memory, thread, tmp, output, totalPlots } = config;
+  const { id, memory, thread, tmp, output, totalPlots } = config
   //kick off process of listing files
-  const child = spawn("chia", [
-    "plots",
-    "create",
-    "-k",
-    "32",
-    "-b",
+  const child = spawn('chia', [
+    'plots',
+    'create',
+    '-k',
+    '32',
+    '-b',
     `${memory}`,
-    "-r",
+    '-r',
     `${thread}`,
-    "-t",
+    '-t',
     `${tmp}`,
-    "-d",
-    `${output}`,
-  ]);
-  const pid = child.pid;
-  const t = new Date();
-  const fileLogName = `./output/${pid}-${t.getFullYear()}-${t.getMonth()}-${t.getDate()}-${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}:${t.getMilliseconds()}.txt`;
+    '-d',
+    `${output}`
+  ])
+  const pid = child.pid
+  const t = new Date()
+  const fileLogName = `./output/${pid}-${t.getFullYear()}-${t.getMonth()}-${t.getDate()}-${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}:${t.getMilliseconds()}.txt`
 
-  processLogMap.set(pid, fileLogName);
+  processLogMap.set(pid, fileLogName)
 
-  const writeStream = fs.createWriteStream(fileLogName);
+  const writeStream = fs.createWriteStream(fileLogName)
 
-  writeStream.write(
-    JSON.stringify({ id, memory, thread, tmp, output, totalPlots })
-  );
-  child.stdout.pipe(writeStream);
+  writeStream.write(JSON.stringify({ id, memory, thread, tmp, output, totalPlots }))
+  child.stdout.pipe(writeStream)
 
-  child.stderr.on("data", function (data) {
-    process.stdout.write(data.toString());
-  });
+  child.stderr.on('data', function (data) {
+    process.stdout.write(data.toString())
+  })
 
-  child.on("close", function (code) {
-    console.log("Finished with code " + code);
-    counter.set(id, counter.get(id) - 1);
+  child.on('close', function (code) {
+    console.log('Finished with code ' + code)
+    counter.set(id, counter.get(id) - 1)
     if (killed) {
-      process.exit(1);
+      process.exit(1)
     } else {
-      const fileLogName = processLogMap.get(pid);
-      fs.renameSync(
-        fileLogName,
-        fileLogName.replace("output/", "output/[DONE]")
-      );
+      const fileLogName = processLogMap.get(pid)
+      fs.renameSync(fileLogName, fileLogName.replace('output/', 'output/[DONE]'))
       process.stdout.write(
         `Plotted ${counter.get(id)}/${totalPlots}, used ${
           (new Date().getTime() - startTime.getTime()) / 1000 / 60
         }`
-      );
-      processLogMap.delete(pid);
+      )
+      processLogMap.delete(pid)
 
-      startMine(config);
+      startMine(config)
     }
-  });
+  })
 }
 
-["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
+;['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach(signal =>
   process.on(signal, () => {
-    console.log(`Got ${signal} signal.`);
-    new Array(...processLogMap.keys()).forEach((pid) => {
-      const fileLogName = processLogMap.get(pid);
-      fs.renameSync(
-        fileLogName,
-        fileLogName.replace("output/", "output/[KILLED]")
-      );
-      process.kill(pid, "SIGKILL");
-    });
-    process.exit(1);
+    console.log(`Got ${signal} signal.`)
+    new Array(...processLogMap.keys()).forEach(pid => {
+      const fileLogName = processLogMap.get(pid)
+      fs.renameSync(fileLogName, fileLogName.replace('output/', 'output/[KILLED]'))
+      process.kill(pid, 'SIGKILL')
+    })
+    process.exit(1)
   })
-);
+)
 
 configs.forEach((config, index) => {
-  const { totalPlots, concurrent } = config;
-  counter.set(index, Number(totalPlots));
+  const { totalPlots, concurrent, delay = 0 } = config
+  counter.set(index, Number(totalPlots))
   for (let i = 0; i < concurrent; i++) {
-    startMine({ id: index, ...config });
+    setTimeout(() => startMine({ id: index, ...config }), delay * 1000 * 60)
   }
-});
+})
